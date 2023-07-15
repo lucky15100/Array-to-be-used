f0b09a6304de7cfe8c2ae8096bdb8ac9dbc9c9ac
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-07-15 23:10:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+const employees = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" }
+];
 
+function PrintDeveloperbyMap() {
+  const developers = employees.map(employee => {
+    if (employee.profession === "developer") {
+      return employee;
+    }
+  });
+
+  console.log(developers);
+}
+
+function PrintDeveloperbyForEach() {
+  employees.forEach(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
+}
+
+function addData() {
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  employees.push(newEmployee);
+
+  console.log(employees);
+}
+
+function removeAdmin() {
+  const filteredEmployees = employees.filter(employee => employee.profession !== "admin");
+  employees = filteredEmployees;
+
+  console.log(employees);
+}
+
+function ConcatinateArray() {
+  const newArray = [
+    { id: 5, name: "michael", age: "22", profession: "developer" },
+    { id: 6, name: "emma", age: "21", profession: "admin" },
+    { id: 7, name: "alex", age: "19", profession: "developer" }
+  ];
+
+  const concatenatedArray = employees.concat(newArray);
+
+  console.log(concatenatedArray);
+}
